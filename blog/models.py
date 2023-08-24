bd00ee261763f58bb6fcdd9be132ab84ee4a6c51
Aarav Poudel <poudelaarav@gmail.com>
2023-08-24 16:06:37 +0545

    dynamic field

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,6 @@ from django.db import models
 class Person(models.Model):
     name = models.CharField(max_length=100)
     address = models.CharField(max_length=255,blank=True,null=True)
-
+    profile_url = models.URLField(blank=True,null=True)
     def __str__(self):
         return self.name
